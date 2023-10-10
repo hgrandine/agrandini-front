bce67d70bf4afca4e18f505d150b5d2806291961
Hugo Grandine <hugog_3@hotmail.com>
2023-10-10 15:34:49 -0300

    finalizando primeiro projeto

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,13 +13,14 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button"
+        (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
-      <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+      <app-housing-location 
+        *ngFor="let housingLocation of filteredLocationList" 
         [housingLocation]="housingLocation">
       </app-housing-location>
     </section>
@@ -34,7 +35,23 @@ export class HomeComponent {
 
   housingService: HousingService = inject(HousingService);
 
-  constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+  filteredLocationList: HousingLocation[] = [];
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+  
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
+
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+
 }
